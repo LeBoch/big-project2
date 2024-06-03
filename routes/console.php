20c59c40f92e178932a3e7e371e4c9920ac6fba3
LeBoch <bochsler.enzo@gmail.com>
2024-06-03 15:00:01 +0200

    Help for Installation ( cron )

--- routes/console.php
+++ routes/console.php
@@ -15,25 +15,27 @@ Schedule::call(function () {
     $data = new \App\Http\Controllers\BikeController();
     $data->fetchData();
     // Appel de cette méthode toutes les 5 minutes
-})->everyFiveMinutes();
+//})->everyFiveMinutes();
+})->everyMinute();
 
 // Appel de notre controller avec la methode pour récupérer toutes les données des parkings de bus
 Schedule::call(function () {
     $data = new \App\Http\Controllers\CarController();
     $data->fetchData();
     // Appel de cette méthode toutes les 5 minutes
-})->everyFiveMinutes();
-
+//})->everyFiveMinutes();
+})->everyMinute();
 // Appel de notre controller avec la methode pour récupérer toutes les données des arrêts de tram
 Schedule::call(function () {
     $data = new \App\Http\Controllers\TramController();
     $data->fetchData();
     // Appel de cette méthode tout les mois
-})->monthly();
-
+//})->monthly();
+})->everyMinute();
 // Appel de notre controller avec la methode pour récupérer toutes les données des arrêts de bus
 Schedule::call(function () {
     $data = new \App\Http\Controllers\BusController();
     $data->fetchData();
     // Appel de cette méthode tout les mois
-})->monthly();
+//})->monthly();
+})->everyMinute();
