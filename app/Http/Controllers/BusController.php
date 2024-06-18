bffaefddb80d21ca3920374263441a68e490e01a
LeBoch <bochsler.enzo@gmail.com>
2024-06-18 23:09:42 +0200

    Résolution problème bus controller

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -15,25 +15,31 @@ class BusController extends Controller
 
     public function fetchData()
     {
-        // Mise en place d'un offset à 1 ( dernier ID récupérer )
-        $offset = 1;
-        $response = Http::get("https://www.herault-data.fr/api/explore/v2.1/catalog/datasets/tam_mmm_tpsreel/records?limit=100&offset={$offset}");
+        // Mise en place d'un offset initial à 0
+        $offset = 0;
+
+        // Premier appel pour obtenir le nombre total d'entrées
+        $response = Http::get("https://www.herault-data.fr/api/explore/v2.1/catalog/datasets/tam_mmm_tpsreel/records?limit=1&offset={$offset}");
         if ($response->successful()) {
             $data = $response->json();
             $totalCount = $data['total_count'];
+        } else {
+            // Gérer le cas où la requête échoue
+            throw new Exception("Failed to fetch data from the API.");
         }
+
         // Truncate les bus pour reset les ID
         Bus::query()->truncate();
-        // Bouclage sur la division par 100 du nombre total d'entrée dans l'api , nous avons un maximum de 100 entrée par appel
-        for ($i = 0; $i < $totalCount / 100; $i++) {
-            // Mise en place de notre offset dans notre url pour pouvoir récupérer les données par rapport au dernier ID
+
+        // Boucle pour récupérer les données par lots de 100
+        while ($offset < $totalCount) {
             $response = Http::get("https://www.herault-data.fr/api/explore/v2.1/catalog/datasets/tam_mmm_tpsreel/records?limit=100&offset={$offset}");
             if ($response->successful()) {
                 $data = $response->json();
                 foreach ($data['results'] as $value) {
-                    // Recuperation uniquement que des bus ( le numéro des lignes de bus commence après 6 )
+                    // Récupération uniquement des bus (le numéro des lignes de bus commence après 6)
                     if ($value['route_short_name'] > 6) {
-                        // Recuperation des données de l'api et insertions des données dans notre BDD
+                        // Récupération des données de l'API et insertion des données dans la BDD
                         $car = new Bus();
                         if (isset($value['stop_coordinates']['lat']) && isset($value['stop_coordinates']['lon'])) {
                             $car->forceFill([
@@ -41,13 +47,16 @@ class BusController extends Controller
                                 'lat' => $value['stop_coordinates']['lat'],
                                 'name' => $value['stop_name'],
                             ])->save();
-                            // On rajoute 100 a notre offset pour pouvoir récuperer les prochaines données
-                            $offset += 100;
                         }
                     }
                 }
-
+                // On rajoute 100 à notre offset pour pouvoir récupérer les prochaines données
+                $offset += 100;
+            } else {
+                // Gérer le cas où la requête échoue
+                throw new Exception("Failed to fetch data from the API at offset {$offset}.");
             }
         }
     }
+
 }
